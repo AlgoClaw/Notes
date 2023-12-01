218c341ab4340f9bdb4d005ca02515ed332bfb3e
AlgoClaw <139091238+AlgoClaw@users.noreply.github.com>
2023-12-01 07:37:12 -0600

    Create ConverseJS_UI_Fix.user.js

---
+++ ConverseJS_UI_Fix.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name        ConverseJS UI Fix
+// @homepageURL null
+// @downloadURL null
+// @updateURL   null
+// @match       *://*chat.syn19.xyz*
+// @description null
+// @version     0.001
+//
+// ==/UserScript==
+//
