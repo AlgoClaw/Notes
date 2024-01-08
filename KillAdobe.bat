5f3aeb7d766026c101913080f5f99f0745094775
AlgoClaw <139091238+AlgoClaw@users.noreply.github.com>
2024-01-08 08:16:57 -0600

    Add files via upload

---
+++ KillAdobe.bat
@@ -0,0 +1,13 @@
+taskkill /f /im adobe*
+taskkill /f /im acro*
+taskkill /f /im armsvc.exe
+taskkill /f /im CCXProcess.exe
+
+#taskkill /f /im acrotray.exe
+#taskkill /f /im AdobeIPCBroker.exe
+#taskkill /f /im AdobeUpdateService.exe
+#taskkill /f /im "Adobe Crash Processor.exe"
+#taskkill /f /im AcroCEF.exe
+#taskkill /f /im Acrobat.exe
+#taskkill /f /im acrotray.exe
+#taskkill /f /im AdobeCollabSync.exe
